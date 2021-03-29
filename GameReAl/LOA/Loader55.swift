a5e5f3b0bf0ed836606130dc5e5e12ed352995ad
jueon <51726172+toy0228@users.noreply.github.com>
2021-03-28 20:05:29 -0400

    Initial Commit

---
+++ GameReAl/LOA/Loader55.swift
@@ -0,0 +1,32 @@
+//
+//  Loader55.swift
+//  GameReAl
+//
+//  Created by sohyeon yun on 3/28/21.
+//
+
+import UIKit
+
+class Loader55
+: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+     
+     This is going to come to Settings View ST.
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GameReAl/MA/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameReAl
+//
+//  Created by sohyeon yun on 3/28/21.
+//
+
+import UIKit
+
+class MainView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
